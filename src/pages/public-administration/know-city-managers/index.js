17717736902b8dbd6fed90803c5523af7c29e121
karol marques <karoldm1579@gmail.com>
2023-01-22 10:02:19 -0300

    feat: api integration for managers page

--- src/pages/public-administration/know-city-managers/index.js
+++ src/pages/public-administration/know-city-managers/index.js
@@ -15,98 +15,63 @@ import { AiOutlineStar } from "react-icons/ai";
 import { AiFillStar } from "react-icons/ai";
 import { StyledHr } from "../../../components/styled-components/StyledHr";
 import { ContainerInputs, ContainerGestorList, ListCardContainer, ContainerColumnCard } from './styles';
-import { ca } from "date-fns/locale";
 import Button from "../../../components/styled-components/form-button";
-
-
-const cargos = [
-	{ titulo: 'cargo 01' },
-	{ titulo: 'cargo 01' },
-	{ titulo: 'cargo 02' },
-	{ titulo: 'cargo 01' }
-];
-
-const gestoresTeste = [
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 02',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 02',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-	{
-		name: 'nome do gestor',
-		cargo: 'cargo 01',
-		dataPosse: '2023-01-01',
-		dataNascimento: '1990-01-01',
-		partido: 'Partido 01 - 01',
-		photo: '/assets/img/profilepic.png',
-	},
-];
+import {api} from '../../../services/api';
 
 const Gestores = (props) => {
 	const [gestores, setGestores] = useState([]);
+	const [gestoresView, setGestoresView] = useState([]);
+	const [funcoes, setFuncoes] = useState({});
+	const [funcoesFilter, setFuncoesFilter] = useState([]);
+	const [partidos, setPartidos] = useState({});
 	const [isFavorite, setIsFavorite] = useState(false);
 
+	const getGestores = async () => {
+		try {
+			const response = await api.get('/gestor');
+			setGestores(response.data);
+			setGestoresView(response.data);
+		} catch(error){
+			console.log(error);
+		}
+	} 
+
+	const getFuncoes = async () => {
+		try {
+			const response = await api.get('/funcao');
+			const data = response.data;
+			let funcaoMap = {};
+			let funcoesFilterArray = [];
+			data.forEach(d => {
+				funcaoMap[d.id] = d.titulo;
+				funcoesFilterArray.push(d.titulo);
+			});
+			setFuncoesFilter(funcoesFilterArray);
+			setFuncoes(funcaoMap);
+		} catch(error){
+			console.log(error);
+		}
+	}
+
+	const getPartidos = async () => {
+		try {
+			const response = await api.get('/partido');
+			const data = response.data;
+			let partidoMap = {};
+			data.forEach(d => {
+				partidoMap[d.id] = `${d.nome} - ${d.sigla}`;
+			});
+			setPartidos(partidoMap);
+		} catch(error){
+			console.log(error);
+		}
+	}
+
 	useEffect(() => {
-		setGestores(gestoresTeste);
+		getGestores();
+		getFuncoes();
+		getPartidos();
+
 		props.data.find(
 			(favoriteX) => favoriteX.id === 46 && setIsFavorite(true)
 		);
@@ -131,10 +96,10 @@ const Gestores = (props) => {
 		setIsFavorite(!isFavorite);
 	};
 
-	const handleFilter = (cargoFilter) => {
-		setGestores(gestoresTeste)
-		const gestoresFiltered = gestoresTeste.filter((gestor) => gestor.cargo === cargoFilter);
-		setGestores(gestoresFiltered);
+	const handleFilter = (funcaoFilter) => {
+		setGestoresView(gestores);
+		const gestoresFiltered = gestores.filter((gestor) => funcoes[gestor.id_funcao] === funcaoFilter);
+		setGestoresView(gestoresFiltered);
 	}
 
 	return (
@@ -207,27 +172,27 @@ const Gestores = (props) => {
 					<ContainerInputs>
 						<b>Filtrar gestor por cargo</b>
 						<select name="cargoList" onChange={(e) => handleFilter(e.target.value)} >
-							{cargos.map((cargo) => {
-								return <option value={cargo.titulo}>{cargo.titulo} </option>
+							{funcoesFilter.map((f) => {
+								return <option value={f}>{f} </option>
 							})}
 						</select>
-						<Button type='button' text='Todos' onClick={() => setGestores(gestoresTeste)} />
+						<Button type='button' text='Todos' onClick={() => setGestoresView(gestores)} />
 					</ContainerInputs>
 					<ContainerGestorList>
 						{
-							gestores.map((gestor, index) => (
-								<ListCardContainer key={index}>
+							gestoresView.map((gestor) => (
+								<ListCardContainer key={gestor.numero}>
 									<img
-										src={gestor.photo}
-										alt={gestor.name}
+										src='/assets/img/profilepic.png'
+										alt={gestor.nome_completo}
 									/>
 									<ContainerColumnCard>
-										<h2> {gestor.name} </h2>
-										<h3> {gestor.cargo} </h3>
-										<h3> {gestor.partido} </h3>
+										<h2> {gestor.nome_completo} </h2>
+										<h3> {funcoes[gestor.id_funcao]} </h3>
+										<h3> {partidos[gestor.id_partido]} </h3>
 										<div>
-											<h4>Data de Posse: {gestor.dataPosse} </h4>
-											<h4>Data de Nascimento: {gestor.dataNascimento} </h4>
+											<h4>Data de Posse: {gestor.data_posse.split("T")[0]} </h4>
+											<h4>Data de Nascimento: {gestor.data_nascimento.split("T")[0]} </h4>
 										</div>
 									</ContainerColumnCard>
 								</ListCardContainer>
--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
-    headers: {
-      Authorization: `Bearer ${localStorage.getItem('@UnespSCity/user')}`
-    }
+    baseURL: 'http://localhost:3000'
   })
